6568ae5e25d7c2f80280d7441e8e7291a2860e4d
Jirka Kremser <jkremser@redhat.com>
2015-08-11 00:39:45 +0200

    dockerfile + bash

--- ircLogBot/src/main/resources/org/hawkular/ircbot/logger.sh
+++ ircLogBot/src/main/resources/org/hawkular/ircbot/logger.sh
@@ -17,7 +17,7 @@ _STYLES="<style>.name {color: red;}</style>"
 #tail -f $1 | grep --line-buffered "PRIVMSG $_CHANNEL" | grep --line-buffered -v "SAFELIST" | grep --line-buffered -v "TARGMAX" | grep --line-buffered -v ">>>PRIVMSG" | awk '{$2=substr($2, 5, index($2, "!") - 5); $3=""; $4="";  print strftime("%b %d %H:%M:%S  %Y", substr($1, 0, 10))$0;system("")}' | awk '{$1="<nobr>"$1; $3=$3"</nobr>"; $4=""; $5="</td><td><span style=\"color:red;\">"$5"</span></td><td>"; $6=substr($6, 2); print "<tr><td>"$0"</td></tr>";system("")}' | sed -u 's/\(https\?:\/\/[^ <]*\)/<a href="\1">\1<\/a>/' >> $_LOG_FILE
 
 # initial copy
-echo "<html><head><title>IRC LOG for $_CHANNEL</title>$_STYLES</head><body><h1>$_CHANNEL</h1><table>"`cat $_INPUT_FILE | awk '{$1="<nobr>"$1; $3=$3"</nobr>"; $4=""; $5="</td><td><span class=\"name\">"$5"</span></td><td>"; $6=substr($6, 2); print "<tr><td>"$0"</td></tr>"}' | sed 's/\(https\?:\/\/[^ <]*\)/<a href="\1">\1<\/a>/'` > $_LOG_FILE
+echo "<html><head><title>IRC LOG for $_CHANNEL</title>$_STYLES</head><body><h1>$_CHANNEL</h1><table>"`cat $_INPUT_FILE | awk '{$1="<nobr>"$1; $3=$3"</nobr>"; $4=""; $5="</td><td><span class=\"name\">"$5"</span></td><td>"; $6; print "<tr><td>"$0"</td></tr>"}' | sed 's/\(https\?:\/\/[^ <]*\)/<a href="\1">\1<\/a>/'` > $_LOG_FILE
 
 # listen and add new changes
-tail -f $1 | awk '{$1="<nobr>"$1; $3=$3"</nobr>"; $4=""; $5="</td><td><span class=\"name\">"$5"</span></td><td>"; $6=substr($6, 2); print "<tr><td>"$0"</td></tr>"}' | sed -u 's/\(https\?:\/\/[^ <]*\)/<a href="\1">\1<\/a>/'` >> $_LOG_FILE
+tail -f $_INPUT_FILE | awk '{$1="<nobr>"$1; $3=$3"</nobr>"; $4=""; $5="</td><td><span class=\"name\">"$5"</span></td><td>"; $6; print "<tr><td>"$0"</td></tr>";system("")}' | sed -u 's/\(https\?:\/\/[^ <]*\)/<a href="\1">\1<\/a>/' >> $_LOG_FILE
